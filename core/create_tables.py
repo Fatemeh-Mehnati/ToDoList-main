9e6adc24a43d1a88ce6569ab1779ed8024c25e60
Fatemeh Mehnati <fatemeh.mehnati20@gmail.com>
2025-12-04 14:42:10 +0330

    fix create_tables

--- core/create_tables.py
+++ core/create_tables.py
@@ -1,6 +1,6 @@
 # core/create_tables.py
-from core.database import engine, Base
-import core.models   # That's enough to load all models.
+from .database import engine, Base
+from . import models   # That's enough to load all models.
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
